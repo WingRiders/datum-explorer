9b3abd903009ad925488a9bece427dfebaac8d7b
TechRiderWR <techrider@wingriders.com>
2025-03-17 09:04:12 +0000

    Add deployment action (#100)
    * Add deployment action
    
    * Add default values for env variables

--- app/src/config.ts
+++ app/src/config.ts
@@ -3,10 +3,10 @@ import {z} from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
   GITHUB_AUTH_TOKEN: z.string().optional(),
-  REPOSITORY_OWNER: z.string().nonempty(),
-  REPOSITORY_NAME: z.string().nonempty(),
-  REPOSITORY_BRANCH: z.string().nonempty(),
-  REPOSITORY_PROJECTS_DIR: z.string().nonempty(),
+  REPOSITORY_OWNER: z.string().default('WingRiders'),
+  REPOSITORY_NAME: z.string().default('cardano-datum-registry'),
+  REPOSITORY_BRANCH: z.string().default('main'),
+  REPOSITORY_PROJECTS_DIR: z.string().default('projects'),
 })
 
 const parsedEnv = envSchema.safeParse(process.env)
